a2d47011b966599b72f352e28d547df8074fa538
ivanhjc <hjcivan@gmail.com>
2020-10-16 18:43:30 +0800

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,21 +5,7 @@
 window.onload = loadBg;
 
 function loadBg() {
-	$(document).ready(function(){
-        $.ajax({
-            url: 'http://twitter.com/status/user_timeline/padraicb.json?count=10',
-            dataType: 'jsonp',
-            success: function(dataWeGotViaJsonp){
-                var text = '';
-                var len = dataWeGotViaJsonp.length;
-                for(var i=0;i<len;i++){
-                    twitterEntry = dataWeGotViaJsonp[i];
-                    text += '<p><img src = "' + twitterEntry.user.profile_image_url_https +'"/>' + twitterEntry['text'] + '</p>'
-                }
-                $('#twitterFeed').html(text);
-            }
-        });
-    })
+
 
 	
     // $.ajax({
